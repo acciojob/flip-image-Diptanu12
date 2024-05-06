c3ab41434b04feb9c27b9fd53239b10fff34f38f
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-05-06 23:15:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// write js code if required
+
+var image = document.getElementById('flip');
+
+
+var initialSrc = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+var finalSrc = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
+
+image.addEventListener('mouseover', function() {
+
+    image.src = finalSrc;
+});
+
+
+image.addEventListener('mouseout', function() {
+
+    image.src = initialSrc;
+});
